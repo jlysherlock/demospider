addf0fda447b144cbf5564d2c41384ad6dcf2603
Luyu Jiang <511645093@qq.com>
2026-01-05 19:17:38 +0800

    1/5

--- demo02/test02.py
+++ demo02/test02.py
@@ -3,8 +3,6 @@ import time
 import requests
 from tqdm import tqdm
 
-
-
 headers = {
     'Host': 'www.spiderdemo.cn',
     'Sec-Ch-Ua-Platform': '"Windows"',
@@ -21,9 +19,8 @@ headers = {
     'Priority': 'u=1, i'
 }
 
-
 cookies = {
-    "sessionid": "6ycyvjk4fby8z0t3l87veiygavbxlxmn"
+    "sessionid": "hy0iomsjkupivfg72vn5xzcti5v9fdwr"
 }
 
 
@@ -32,36 +29,41 @@ def get_hex_md5_signature(timestamp, page):
     # 确保 timestamp 和 page 都是字符串格式
     data = timestamp + str(page)
 
-    # 盐值
-    # salt = b"\xa3\xac\xa1\xa3\x66\x64\x6a\x66\x2c\x6a\x6b\x67\x66\x6b\x6c"
-    salt ="£¬¡£fdjf,jkgfkl"
+    # 盐值为字符串
+    salt = "£¬¡£fdjf,jkgfkl"
+    # 不能字节串相加，只能字符串相加后再编码成字节串
     data_with_salt = (data + salt).encode("latin1")
-
     md5_hash = hashlib.md5(data_with_salt)
+
+    # 盐值为字节串
+    # salt = b"\xa3\xac\xa1\xa3\x66\x64\x6a\x66\x2c\x6a\x6b\x67\x66\x6b\x6c"
+    # md5_hash = hashlib.md5()
+    # md5_hash.update(data.encode("latin1"))
+    # md5_hash.update(salt)
     # 4. 获取 16 进制的加密结果 (对应 hex_md5)
     return md5_hash.hexdigest()
+
 
 def get_page_data(i):
     timestamp = str(int(time.time() * 1000))
     sign = get_hex_md5_signature(timestamp, i)
-    print(sign)
+    # print(sign)
     url = f"https://www.spiderdemo.cn/ob/api/ob_challenge1/page/{i}/"
     params = {
         "challenge_type": "ob_challenge1",
         "sign": sign,
         "timestamp": timestamp
     }
-    response = requests.get(url, headers=headers, cookies=cookies, params=params)
-    print(response.text)
+    response = requests.get(url,headers=headers, cookies=cookies, params=params)
+    # print(response.text)
     data = response.json()["page_data"]
 
     return sum(data)
 
 
-
 if __name__ == '__main__':
     total = 0
-    for i in tqdm(range(1, 2)):
+    for i in tqdm(range(1, 101)):
         total += get_page_data(i)
 
     print(total)
